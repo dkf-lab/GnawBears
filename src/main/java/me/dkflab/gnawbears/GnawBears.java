6aa8e16b89dc46d64ee8be3efff94c15ec359904
dkf-lab <pxlbuisness@gmail.com>
2021-12-07 11:09:05 +1030

    Initial commit

---
+++ src/main/java/me/dkflab/gnawbears/GnawBears.java
@@ -0,0 +1,33 @@
+package me.dkflab.gnawbears;
+
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+public final class GnawBears extends JavaPlugin {
+
+    String wallet = "test";
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    public String getPlayerWallet(UUID uuid) {
+        // UUID is a unique identifier for each player
+        return wallet;
+    }
+
+    public List<String> getPlayerNFTs(UUID uuid) {
+        // Would probably make an NFT() object
+        return Collections.singletonList("GNAWBEARS001");
+    }
+}
